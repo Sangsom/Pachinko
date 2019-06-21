70bcef5341d30aec7d4819cabd12ba4ecb8876a7
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-06-21 10:22:27 +0300

    Adding bouncers on screen

--- Pachinko/GameScene.swift
+++ Pachinko/GameScene.swift
@@ -19,11 +19,11 @@ class GameScene: SKScene {
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         addChild(background)
 
-        let bouncer = SKSpriteNode(imageNamed: "bouncer")
-        bouncer.position = CGPoint(x: 512, y: 0)
-        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
-        bouncer.physicsBody?.isDynamic = false
-        addChild(bouncer)
+        makeBouncer(at: CGPoint(x: 0, y: 0))
+        makeBouncer(at: CGPoint(x: 256, y: 0))
+        makeBouncer(at: CGPoint(x: 512, y: 0))
+        makeBouncer(at: CGPoint(x: 768, y: 0))
+        makeBouncer(at: CGPoint(x: 1024, y: 0))
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
